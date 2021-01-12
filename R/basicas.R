22aa85c0ffd8a0778a0aca13919587bc89f75428
egoipse <gimenezd@gmail.com>
2021-01-12 05:46:52 -0300

    6 funciones, documentación en /man completa

--- R/basicas.R
+++ R/basicas.R
@@ -1,24 +1,41 @@
 `%notin%` <- Negate(`%in%`)
 
-#' Determinar qué tipo de objeto con precisión
+#' Determinar el tipo de objeto
 #'
-#' @param x
-#' @param ...
+#' @description Indica en el terminal cuál es el tipo del objeto que se
+#'   introduce como argumento. Lo hace con máxima precisión. Las alternativas
+#'   de R base (`class` y `typeof`) no detectan diferencias entre distintos
+#'   tipos de vectores. Y `tipeof` no distingue entre listas y data frames.
+#'   Esta función subsana ambas limitaciones.
 #'
-#' @return vector
+#' @param x **Un objeto cualquiera**, cuyo tipo se desee saber.
+#'
+#' @return Texto en el terminal indicando el tipo de objeto.
+#' @details El valor que arroja la función, al ser sólo texto en el terminal,
+#'   ***no se puede guardar*** a su vez como un nuevo objeto.
+#'
 #' @export
 #'
 #' @examples
 #' bbdd <- data(mtcars)
 #' que.es(bbdd)
-que.es <- function(x, ...) {
+#'
+#' x <- expression(pi * 5)
+#' que.es(x)
+que.es <- function(x) {
   if (!exists(as.character(substitute(x)))) {
     cat("No existe. No es un objeto definido o asignado. Def\u00ednelo primero con \"x <- algo \" ")
   }
   if (nargs() > 1) {
     cat("Se puede comprobar un \u00fanico objeto a la vez. Ingresa s\u00f3lo uno")
-  } else if (is.vector(x) & !is.matrix(x)) {
-    cat("Es VECTOR")
+  } else if (is.vector(x) & !is.matrix(x) & !is.expression(x)) {
+
+    if(is.logical(x)) cat("Es VECTOR de valores l\u00f3gicos (T/F)")
+
+    else if(is.character(x)) cat("Es VECTOR de caracteres")
+
+    else  if(is.numeric(x)) cat("Es VECTOR de valores num\u00e9ricos")
+
   }
   else if (is.matrix(x)) {
     cat("Es MATRIZ")
@@ -41,7 +58,7 @@ que.es <- function(x, ...) {
   else if (is.environment(x)) {
     cat("Es un ENVIRONMENT")
   }
-  else if (is.language(x)) {
+  else if (is.language(x) & !is.expression(x)) {
     cat("Es un LENGUAJE")
   }
   else if (is.expression(x)) {
@@ -54,17 +71,49 @@ que.es <- function(x, ...) {
 
 #' Incorporar objetos a una lista
 #'
-#' @param lista
-#' @param obj
-#' @param pos
-#' @param rm
-#' @param ...
+#' @description Sirve para agregar objetos a una lista de forma rápida controlando la posición
 #'
-#' @return
+#' @param lista **Una lista**. Es necesario que exista previamente en el entorno
+#'   global (GlobalEnvir).
+#' @param obj **Objeto de cualquier tipo**. Es necesario que exista previamente en
+#'   el entorno global (GlobalEnvir).
+#' @param pos **Un número**. Puede asumir dos valores:
+#'      **0** para que el objeto quede al final de la lista (*predeterminado*:
+#'      `lista[[length(lista)]])`; o
+#'      **1** para que quede en el primer lugar de la lista (`lista[[1]]`).
+#' @param rm **Valor lógico**. Si es `TRUE` (*predeterminado*), el objeto será eliminado del entorno
+#'   global (GlobalEnvir) después de ser agregado a la lista. Si es `FALSE`,
+#'   el objeto será añadido a la lista sin ser eliminado del entorno global.
+#'
+#' @details Si `lista` no existe en el entorno global cuado se aplica la
+#'   función, arrojará error. Debe crearse antes. Puede ser una lista que ya
+#'   tenga otros objetos o una lista vacía: `lista <- list()`.
+#'
+#'   Si `obj` no existe antes de aplicar la función, arrojará error. Debe
+#'   existir antes en el entorno global.
+#'
+#'   Si `listar()` se usa dentro de una función personalizada, debe definirse
+#'   `rm = FALSE`. De lo contrario, arrojará error.
+#'
+#' @return Una *`lista`* con el *`obj`* incorporado en la posición indicada con el
+#'   argumento *`pos`*.
 #' @export
 #'
 #' @examples
-listar <- function(lista, obj, pos = 0, rm = T, ...) {
+#' mis_cosas <- list(
+#'    libros = c("El péndulo de Foucault", "Germinal", "Artificios", "Angela's Ashes"),
+#'    vinilos = c("Abbey Road", "Kind of blue", "Nevermind",
+#'     "Bigger, better, faster, more", "Parte de la religión")
+#'   )
+#'
+#' computadoras <- c("Notebook", "Workstation", "Raspberry Pi")
+#'
+#' # Agregando el objeto al final de la lista y conservándolo en el entorno global:
+#'   mis_cosas <- listar(mis_cosas, computadoras, pos = 0, rm = FALSE)
+#'
+#' # Agregando el objeto al primer lugar de la lista y eliminándolo del entorno global:
+#'   mis_cosas <- listar(mis_cosas, computadoras, pos = 1, rm = TRUE)
+listar <- function(lista, obj, pos = 0, rm = T) {
 
   if (pos %notin% c(1, 0, "final", "inicio")) {
     stop("La posici\u00f3n en la lista s\u00f3lo admite cuatro valores: \"1\" o \"inicio\" para agregar el objeto al principio de la lista; \"0\" o \"final\" para agregarlo al final. Si no ingresas el argumento, el objeto se agrega al final.")
--- R/transformar.R
+++ R/transformar.R
@@ -1,19 +1,49 @@
 #' Convertir columnas de un data frame a números
 #'
-#' @param df
-#' @param columnas
-#' @param ...
+#' @description Convierte las columnas de un data frame a vectores numéricos.
 #'
-#' @return
+#' @param df **Un data frame**. La función admite sólo data frames, no objetos
+#'   derivados, como tibles.
+#' @param columnas **Un vector con las columnas a transformar**. Puede ser
+#'   vector numérico con el índice de las columnas (`columnas = 2:4`) o
+#'   vector de caracteres con el nombre de las columnas
+#'   (`columnas = c("a", "b"`)).
+#'
+#' @return Un dataframe con las columnas del argumento convertidas a vectores
+#'   numéricos. Si las columnas tienen caracteres y no números, sus valores serán
+#'   convertidos a NAs.
 #' @export
 #'
+#' @details Muchas operaciones aplicadas a data frames, fundamentalmente a
+#'  través de las funciones de `dplyr`, transforman las columnas numéricas a
+#'  columnas de caracteres. Esta función convierte las columnas de caracteres en
+#'  columnas numéricas.
+#'
+#'  El requisito para el funcionamiento de esta función: que **todos los elementos
+#'  o ítems del vector de la columna tengan sólo números entre las comillas**. Si
+#'  existe al menos un ítem o elemento del vector con al menos un caracter no
+#'  numérico, como una coma, un punto o un signo monetario ($, €, £, 元), la
+#'  función transformará todos los valores del vector en NAs.
+#'
+#'  Sólo se admiten objetos del tipo data frame en el argumento `df`. Las
+#'  `tibles` u otros objetos derivados de data frames podrían arrojar error. La
+#'  función intenta coercionar el objeto ingresado en el argumento `df` a la
+#'  clase `data frame` antes de iniciar cualquier transformación.
+#'
 #' @examples
-#' # Teniendo un data frame con columnas que tienen números pero, por diversas
-#'  circunstancias, como vectores de caracteres....
-#' cuadro = data.frame (a = as.character(c(1:5)), b = as.character(c(6:10)))
-#' # a_numeros generará
-#' # cuadro <- a_numeros(cuadro, 1:2)
-a_numeros <- function(df, columnas, ...) {
+#' cuadro <- data.frame (a = as.character(c(1:5)), b = as.character(c(6:10)))
+#'
+#' # La función convertirá las columnas señaladas en el argumento a formato numérico:
+#'   cuadro <- a_numeros(df = cuadro, columnas = 1:2)
+#'
+#' # También es posible aplicar la función a llamando las columnas por sus nombres:
+#'   cuadro <- a_numeros(cuadro, c("a", "b"))
+#'
+#' # Las columnas con vectores de caracteres que tengan algo más que números generará NAs.
+#'   cuadro <- cbind(cuadro, letters[1:5])
+#'   cuadro <- a_numeros(cuadro, 3) # La tercera columna, de caracteres, queda sólo con NAs
+#' @family funciones para transformar columnas de data frames
+a_numeros <- function(df, columnas) {
   df <- as.data.frame(df)
 
   df[,columnas] <-  sapply(df[,columnas], as.numeric)
@@ -24,21 +54,32 @@ a_numeros <- function(df, columnas, ...) {
 
 #' Convertir columnas de un data frame a caracteres
 #'
-#' @param df
-#' @param columnas
-#' @param ...
+#' @description Convierte las columnas de un data frame a vectores de caracteres.
 #'
-#' @return
+#' @param df **Un data frame**. La función admite sólo data frames, no objetos
+#'   derivados, como tibles.
+#' @param columnas **Un vector con las columnas a transformar**. Puede ser
+#'   vector numérico con el índice de las columnas (`columnas = 2:4`) o
+#'   vector de caracteres con el nombre de las columnas
+#'   (`columnas = c("a", "b"`)).
+#'
+#' @return Un dataframe con las columnas del argumento convertidas a vectores
+#'   de caracteres.
 #' @export
 #'
+#' @details Sólo se admiten objetos del tipo data frame en el argumento `df`. Las
+#'  `tibles` u otros objetos derivados de data frames podrían arrojar error. La
+#'  función intenta coercionar el objeto ingresado en el argumento `df` a la
+#'  clase `data frame` antes de iniciar cualquier transformación.
+#'
 #' @examples
-#' # Las columnas se pueden ingresar por su índice...
-#' mtcars <- a_caracteres(df = mtcars, columnas = 3:7)
+#' # Las columnas se pueden ingresar por su índice:
+#'   mtcars <- a_caracteres(df = mtcars, columnas = 3:7)
 #'
 #' # O también por el nombre:
-#' mtcars <- a_caracteres(df = mtcars, columnas = c("hp", "wt", "am"))
-#'
-a_caracteres <- function(df, columnas, ...) {
+#'   mtcars <- a_caracteres(df = mtcars, columnas = c("hp", "wt", "am"))
+#' @family funciones para transformar columnas de data frames
+a_caracteres <- function(df, columnas) {
   df <- as.data.frame(df)
 
   df[,columnas] <- sapply(df[,columnas], as.character)
@@ -47,15 +88,38 @@ a_caracteres <- function(df, columnas, ...) {
 }
 
 
-#' Convertir los NA a ceros en columnas de data frame
+#' Convertir los NA del data frame a ceros en las columnas seleccionadas
 #'
-#' @param df
-#' @param columnas
+#' @description Convierte los valores NA de las columnas de un data frame a
+#'   ceros.
 #'
-#' @return
+#' @param df **Un data frame**. La función admite sólo data frames, no objetos
+#'   derivados, como tibles.
+#' @param columnas **Un vector con las columnas a transformar**. Puede ser
+#'   vector numérico con el índice de las columnas (`columnas = 2:4`) o
+#'   vector de caracteres con el nombre de las columnas
+#'   (`columnas = c("a", "b"`)).
+#'
+#' @return Un dataframe con los valores NA de las columnas del argumento
+#'   convertidas a valores cero (0).
 #' @export
 #'
+#' @details Para que esta función no arroje errores, es necesario convertir
+#'   antes las columnas a formato numérico. Puede usarse la función
+#'   \code{\link{a_numeros}}.
+#'
+#'  Sólo se admiten objetos del tipo data frame en el argumento `df`. Las
+#'  `tibles` u otros objetos derivados de data frames podrían arrojar error. La
+#'  función intenta coercionar el objeto ingresado en el argumento `df` a la
+#'  clase `data frame` antes de iniciar cualquier transformación.
+#'
 #' @examples
+#' datos <- data.frame(a = c(10,15,NA, 25), b = c(NA, NA, 30, 40))
+#' (datos <- a_cero(datos, 1:2))
+#'
+#' # También es posible aplicar la función llamando a las columnas por sus nombres:
+#'   (datos <- a_cero(datos, c("a", "b")))
+#' @family funciones para transformar columnas de data frames
 a_cero <- function(df, columnas) {
   df <- as.data.frame(df)
 
